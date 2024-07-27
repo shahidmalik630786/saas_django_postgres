a3d7115edf18088e4120fef8ac9d72e08f66365a
shahid sallauddin malik <98082214+shahidmalik630786@users.noreply.github.com>
2024-07-27 09:55:37 +0530

    Initial commit

---
+++ src/cfehome/cfehome/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home_page_view(request):
+    page_visit = PageVisit.objects.filter(path=request.path)
+    page_visit_total = PageVisit.objects.all()
+    page_visit_create = PageVisit.objects.create()
+    my_context = {
+        'page_visit': page_visit
+    }
+    return render(request, 'home.html', my_context)
