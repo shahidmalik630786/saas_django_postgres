1b90ad485ea3b6293d14c7daea5825917ab79093
shahid sallauddin malik <98082214+shahidmalik630786@users.noreply.github.com>
2024-07-30 10:00:26 +0530

    makemigrations and migrate

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -87,12 +87,7 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 CON_MAX_AGE =  config("CON_MAX_AGE", cast=int, default=30)
 DATABASE_URL = config("DATABASE_URL", cast=str, default=None)
 if DATABASE_URL is not None:
